b98cf3bf31622ca654a1e7d09109108999c37fbe
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-02-01 10:33:39 +0000

    CI added

--- src/fizzBuzz.test.ts
+++ src/fizzBuzz.test.ts
@@ -4,6 +4,7 @@ test("Do fizzBuzz.", () => {
 
     expect(fizzBuzz(15)).toEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz");
     expect(fizzBuzz(0)).toEqual("");
+    expect(fizzBuzz(-5)).toEqual("");
     expect(fizzBuzz()).toEqual("");
 
 })
