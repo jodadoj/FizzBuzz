80359f31d8660e5cd5aff58ccc92643db313bb3d
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-02-01 12:05:19 +0000

    further tests

--- src/fizzBuzz.test.ts
+++ src/fizzBuzz.test.ts
@@ -5,6 +5,7 @@ test('Do fizzBuzz.', () => {
     '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz'
   );
   expect(fizzBuzz(1.5)).toBe('1');
+  expect(fizzBuzz(6.5)).toBe('1, 2, Fizz, 4, Buzz, Fizz');
   expect(fizzBuzz(0)).toBe('');
   expect(fizzBuzz(-5)).toBe('');
   expect(fizzBuzz()).toBe('');
