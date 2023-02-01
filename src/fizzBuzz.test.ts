16934d5e2270cb8e4f0e019bf6da5ca8a0355281
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-02-01 10:44:50 +0000

    Husky attempt

--- src/fizzBuzz.test.ts
+++ src/fizzBuzz.test.ts
@@ -1,10 +1,10 @@
-import { fizzBuzz } from "./fizzBuzz";
+import { fizzBuzz } from './fizzBuzz';
 
-test("Do fizzBuzz.", () => {
-
-    expect(fizzBuzz(15)).toEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz");
-    expect(fizzBuzz(0)).toEqual("");
-    expect(fizzBuzz(-5)).toEqual("");
-    expect(fizzBuzz()).toEqual("");
-
-})
+test('Do fizzBuzz.', () => {
+  expect(fizzBuzz(15)).toEqual(
+    '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz'
+  );
+  expect(fizzBuzz(0)).toEqual('');
+  expect(fizzBuzz(-5)).toEqual('');
+  expect(fizzBuzz()).toEqual('');
+});
