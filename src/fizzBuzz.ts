9f4f14b4d4f140c4dc3f49639d369252976b107c
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-02-01 12:02:01 +0000

    Made bad inputs clearer by adding log

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,19 +1,24 @@
 export function fizzBuzz(input = 0): string {
-  if (input <= 0) {
+  //if no entry or invalid range or N
+  if (input <= 0 || typeof input !== 'number') {
+    console.log('Please enter a number above zero. Inputs will be rounded up.');
     return '';
   }
   const finalResult = [];
   for (let current = 1; current <= input; current++) {
+    //if divisible by both
     if (current % 3 === 0 && current % 5 === 0) {
       const result = 'FizzBuzz, ';
       finalResult.push(result);
     } else if (current % 3 === 0) {
+      //otherwise check divisble by either
       const result = 'Fizz, ';
       finalResult.push(result);
     } else if (current % 5 === 0) {
       const result = 'Buzz, ';
       finalResult.push(result);
     } else {
+      //finally just print the number if n
       const result = String(current) + ', ';
       finalResult.push(result);
     }
